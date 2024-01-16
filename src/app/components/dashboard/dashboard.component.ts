b78e45b407fa5c8bd81d14d51ffa7fd562a42df2
Pravin Adhav <pravinadhav4945@gmail.com>
2024-01-16 19:15:28 +0530

    added a dashboard component to web app

---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,22 @@
+import { Component,OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+  user:string='';
+
+  constructor(private router:Router){}
+  ngOnInit(): void {
+  this.user=sessionStorage.getItem('email') as string;    
+  }
+logOut()
+{
+  sessionStorage.clear();
+  this.router.navigate(['/login'])
+}
+
+}
