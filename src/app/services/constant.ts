e5ba4e1a0330aa4b437f42d269d4f25c9d981767
Pravin Adhav <pravinadhav4945@gmail.com>
2024-03-12 14:01:28 +0530

    updated the build size and changed the DB_URL

--- src/app/services/constant.ts
+++ src/app/services/constant.ts
@@ -1,2 +1,2 @@
-export const DB_URL:string="http://localhost:8001/";
-// export const DB_URL:string="https://personalexpensetrackerbackend.onrender.com/"
+// export const DB_URL:string="http://localhost:8001/";
+export const DB_URL:string="https://personalexpensetrackerbackend.onrender.com/"
